dbde67e0431b8c7fbf0466336b985e393e99805d
Thomas <tbeard31@gmail.com>
2022-10-05 12:40:59 -0400

    knowledge check_1

---
+++ knowledge check_1.py
@@ -0,0 +1,12 @@
+print('Hello, World!')
+
+mylist = ['blue', 'green', 'yellow', 'red', 'purple']
+mylist.append('violet')
+print(mylist[1])
+
+mydictionary = {'key1':'value1', 'key2':'value2'}
+mydictionary['key3'] = 'value3'
+print(mydictionary['key1'])
+
+thistuple = ('elephant', 'bear', 'lion', 'owl')
+print(thistuple[3])
